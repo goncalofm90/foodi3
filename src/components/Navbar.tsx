8076916e68c6031f38b29417e01a8e19e8cea20e
goncalofm90 <goncalofm90@gmail.com>
2026-01-07 15:00:49 +0000

    update navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,8 @@
 export default function Navbar() {
   return (
-    <nav className="bg-blue-500 p-4">
-      <h1 className="text-xl font-bold">Foodi3 Navbar</h1>
+    <nav>
+      <a href="/dishes">Dishes</a>
+      <a href="/cocktails">Cocktails</a>
     </nav>
   );
 }
