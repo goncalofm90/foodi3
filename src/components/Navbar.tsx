85ab8066aac636343ade87d7327bdb533dbb01a3
goncalofm90 <goncalofm90@gmail.com>
2026-01-07 15:59:22 +0000

    link to home

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,10 +4,16 @@ import Link from "next/link";
 export default function Navbar() {
   return (
     <nav className="bg-gray-800 text-white p-4 flex justify-between">
-      <h1 className="text-xl font-bold">Foodi3</h1>
+      <Link href="/" className="hover:underline">
+        <h1 className="text-xl font-bold">Foodi3</h1>
+      </Link>
       <div className="space-x-4">
-        <Link href="/dishes" className="hover:underline">Dishes</Link>
-        <Link href="/cocktails" className="hover:underline">Cocktails</Link>
+        <Link href="/dishes" className="hover:underline">
+          Dishes
+        </Link>
+        <Link href="/cocktails" className="hover:underline">
+          Cocktails
+        </Link>
       </div>
     </nav>
   );
