9f6e8b8ac45aa7a10b3552b564a7f691943ece28
goncalofm90 <goncalofm90@gmail.com>
2026-01-19 11:03:18 +0000

    create protected route component

---
+++ src/components/ProtectedRoute.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { ReactNode, useEffect, useState } from "react";
+import { account } from "@/lib/client";
+import { useRouter } from "next/navigation";
+
+interface ProtectedRouteProps {
+  children: ReactNode;
+}
+
+export default function ProtectedRoute({ children }: ProtectedRouteProps) {
+  const [loading, setLoading] = useState(true);
+  const [authenticated, setAuthenticated] = useState(false);
+  const router = useRouter();
+
+  useEffect(() => {
+    const checkAuth = async () => {
+      try {
+        await account.get(); // throws if not logged in
+        setAuthenticated(true);
+      } catch {
+        router.replace("/auth");
+      } finally {
+        setLoading(false);
+      }
+    };
+    checkAuth();
+  }, [router]);
+
+  if (loading) return <p className="p-4">Loading...</p>;
+  if (!authenticated) return null;
+
+  return <>{children}</>;
+}
