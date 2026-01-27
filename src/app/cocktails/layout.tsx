1c613bcb9f453cdafcb368728fd48df035a2d2d9
goncalofm90 <goncalofm90@gmail.com>
2026-01-27 12:14:44 +0000

    navbar fix

--- src/app/cocktails/layout.tsx
+++ src/app/cocktails/layout.tsx
@@ -1,7 +1,12 @@
-// src/app/cocktails/layout.tsx
 import { ReactNode } from "react";
+import Navbar from "@/components/Navbar";
 import PrivateWrapper from "@/components/PrivateWrapper";
 
 export default function CocktailsLayout({ children }: { children: ReactNode }) {
-  return <PrivateWrapper>{children}</PrivateWrapper>;
+  return (
+    <PrivateWrapper>
+      <Navbar />
+      <main className="p-4">{children}</main>
+    </PrivateWrapper>
+  );
 }
--- src/app/dishes/layout.tsx
+++ src/app/dishes/layout.tsx
@@ -1,6 +1,12 @@
 import { ReactNode } from "react";
+import Navbar from "@/components/Navbar";
 import PrivateWrapper from "@/components/PrivateWrapper";
 
 export default function DishesLayout({ children }: { children: ReactNode }) {
-  return <PrivateWrapper>{children}</PrivateWrapper>;
+  return (
+    <PrivateWrapper>
+      <Navbar />
+      <main className="p-4">{children}</main>
+    </PrivateWrapper>
+  );
 }
