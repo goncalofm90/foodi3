28116eaad01e5bd014c701665a351afd14daf6b4
goncalofm90 <goncalofm90@gmail.com>
2026-02-09 18:27:45 +0000

    meta img

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,25 @@ import { ToastProvider } from "@/contexts/ToastContext";
 export const metadata: Metadata = {
   title: "Foodi3",
   description: "Search and save recipes and cocktails",
+  openGraph: {
+    title: "Foodi3",
+    description: "Search and save recipes and cocktails",
+    images: [
+      {
+        url: "/foodi3.jpg",
+        width: 1200,
+        height: 630,
+        alt: "Foodi3 Logo",
+      },
+    ],
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Foodi3",
+    description: "Search and save recipes and cocktails",
+    images: ["/foodi3.jpg"],
+  },
 };
 
 export default function RootLayout({
